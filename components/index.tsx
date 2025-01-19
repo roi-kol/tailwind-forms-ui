c470fb0c22145f847a170ef95a1e11514337d093
Roi Kol <roikolby@gmail.com>
2025-01-19 11:02:28 +0200

    index

---
+++ components/index.tsx
@@ -0,0 +1,26 @@
+// src/index.js
+
+import AutocompleteForm from './Form/Fields/AutocompleteForm';
+import ButtonForm from './Form/Fields/ButtonForm';
+
+import DropdownForm from "./Form/Fields/DropdownForm";
+import RadioGroupForm from "./Form/Fields/RadioGroupForm";
+import CheckboxGroupForm from "./Form/Fields/CheckboxGroupForm";
+import TextAreaForm from "./Form/Fields/TextAreaForm";
+import InputForm from "./Form/Fields/InputForm";
+import FileUploadForm from "./Form/Fields/FileUploadForm";
+
+// import './styles.css'; // If you have any global styles
+
+// Exporting the component so it can be used in other projects
+export { AutocompleteForm };
+export { ButtonForm };
+export { DropdownForm };
+export { RadioGroupForm };
+export { CheckboxGroupForm };
+export { TextAreaForm };
+export { InputForm };
+export { FileUploadForm };
+
+// Optionally, you can also export other components or utilities
+// export { AnotherComponent } from './components/AnotherComponent';

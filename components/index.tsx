b1fb708d9afdcf947a77f3c70375c9cddeda2923
Roi Kol <roikolby@gmail.com>
2025-05-11 12:15:00 +0300

    fix: update version to 1.0.23 and import Option type in index.tsx

--- components/index.tsx
+++ components/index.tsx
@@ -2,7 +2,7 @@
 
 import AutocompleteForm from './Form/Fields/AutocompleteForm';
 import ButtonForm from './Form/Fields/ButtonForm';
-
+import { Option } from './Form/Fields/AutocompleteForm'; // Importing the Option type
 import DropdownForm from "./Form/Fields/DropdownForm";
 import RadioGroupForm from "./Form/Fields/RadioGroupForm";
 import CheckboxGroupForm from "./Form/Fields/CheckboxGroupForm";
@@ -13,7 +13,7 @@ import DatePickerForm from "./Form/Fields/DatePickerForm";
 // import './styles.css'; // If you have any global styles
 
 // Exporting the component so it can be used in other projects
-export { AutocompleteForm };
+export { AutocompleteForm, };
 export { ButtonForm };
 export { DropdownForm };
 export { RadioGroupForm };
@@ -22,5 +22,6 @@ export { TextAreaForm };
 export { InputForm };
 export { FileUploadForm };
 export { DatePickerForm };
+export type { Option }; // Exporting the Option type for use in other components
 // Optionally, you can also export other components or utilities
 // export { AnotherComponent } from './components/AnotherComponent';
